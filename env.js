3256fdf72c7d745bcac5e572d44851e7238341b9
zkxil <zkxil@163.com>
2019-02-14 16:16:37 +0800

    change port

--- env.js
+++ env.js
@@ -2,7 +2,7 @@
 var clusters = [
    {
      NAME:"prod",
-     KAFKA_CONNECT: "http://39.105.135.11:8083",
+     KAFKA_CONNECT: "http://39.105.135.11:50015",
      KAFKA_TOPICS_UI: "http://39.105.135.11:50001",
      KAFKA_TOPICS_UI_ENABLED: true ,
      COLOR: "#141414"
