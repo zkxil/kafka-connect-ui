f775e2c9ae5e2c8358f9b5f2e0d3fc8509ee4310
zkxil <zkxil@163.com>
2019-02-14 15:12:10 +0800

    init

--- env.js
+++ env.js
@@ -2,18 +2,9 @@
 var clusters = [
    {
      NAME:"prod",
-     KAFKA_CONNECT: "http://kafka-connect.prod.url",
+     KAFKA_CONNECT: "0.0.0.0:8083",
      KAFKA_TOPICS_UI: "http://kafka-topics-ui.url",
      KAFKA_TOPICS_UI_ENABLED: true ,
      COLOR: "#141414"
-   },
-   {
-     NAME:"dev",
-     KAFKA_CONNECT: "http://kafka-connect.dev.url",
-     KAFKA_TOPICS_UI_ENABLED: false
-   },
-   {
-     NAME:"local",
-     KAFKA_CONNECT: "http://kafka-connect.local.url",
    }
 ]
